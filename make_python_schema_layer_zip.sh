3ccaa8a03fa0f69f519b17533140fac37096a04b
Manuel Guillén <39125887+ManuelGuillen89@users.noreply.github.com>
2021-12-26 18:51:30 -0300

    base

---
+++ make_python_schema_layer_zip.sh
@@ -0,0 +1,4 @@
+echo "STARTED!" & 
+cp commands_and__events_schema.py ./python_schena_layer/python & 
+zip -r ./python_schena_layer/commands_and__events_schema.zip ./python_schena_layer/python & 
+echo "DONE!" 
